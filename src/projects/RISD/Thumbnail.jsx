1da1fd4ab09d32f48c83ae8d5d3e0aa7c0572c70
Melanie <itsmelaniepatterson@gmail.com>
2026-05-31 16:29:32 -0400

    Fix RISD thumbnail missing images

--- src/projects/RISD/Thumbnail.jsx
+++ src/projects/RISD/Thumbnail.jsx
@@ -1,12 +1,10 @@
-// import img1 from "./images/melanie-patterson-risd.webp";
-import img2 from "./images/melanie-patterson-risd-email-1.webp";
 import img3 from "./images/03.webp";
 
 export default function Thumbnail({ hovered }) {
   return (
     <div style={{ position: "relative", width: "100%", aspectRatio: "4/3" }}>
       <img
-        src={img1}
+        src={img3}
         alt=""
         style={{
           position: "absolute",
@@ -14,30 +12,6 @@ export default function Thumbnail({ hovered }) {
           width: "100%",
           height: "100%",
           objectFit: "cover",
-        }}
-      />
-      <img
-        src={img2}
-        alt=""
-        style={{
-          position: "absolute",
-          bottom: "5%",
-          right: "5%",
-          width: "45%",
-          height: "auto",
-          transition: "transform 0.4s ease",
-          transform: hovered ? "translate(4px, -4px)" : "none",
-        }}
-      />
-      <img
-        src={img3}
-        alt=""
-        style={{
-          position: "absolute",
-          top: "5%",
-          left: "5%",
-          width: "30%",
-          height: "auto",
         }}
       />
     </div>
